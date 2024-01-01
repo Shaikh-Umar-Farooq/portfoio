a73d83242372667a6806a71418be6faa050af7bf
Shaikh-Umar-Farooq <ushaikh_be21@thapar.edu>
2024-01-01 21:35:11 +0530

    dark them completed

--- script.js
+++ script.js
@@ -29,3 +29,30 @@ displayTime();
 
 // Update time every second
 setInterval(displayTime, 1000);
+
+// Array of skills
+const skillsList = [
+    'Wire-framing', 'Prototyping', 'ui','mobile app design','website design','User Research', 'Usability Testing',
+    'Interaction Design', 'User Flow Design', 'Information Architecture','ux',
+    'C/C++','User Interface Design',  'JavaScript', 'HTML', 'CSS','React js','node js', 'Express js',
+    'MongoDB', 'MySQL', 'Analytics'
+  ];
+  
+  // Function to add skills as skillcard divs to the content_overflow div
+  function addSkills() {
+    const contentOverflow = document.querySelector('#skills .content_overflow .skillcontainer');
+  
+    skillsList.forEach(skill => {
+      const skillCard = document.createElement('div');
+      skillCard.className = 'skillcard';
+      skillCard.textContent = skill;
+      contentOverflow.appendChild(skillCard);
+    });
+  }
+  
+  // Call the function to add skills to the content_overflow div
+  addSkills();
+
+
+  const currentYear = new Date().getFullYear();
+  document.getElementById('currentYear').textContent = currentYear;
