77372fe787d68a9d7d792deb06dd7fed7b042874
Shaikh-Umar-Farooq <ushaikh_be21@thapar.edu>
2024-01-02 00:26:23 +0530

    Add theme switcher functionality and styles

--- script.js
+++ script.js
@@ -56,3 +56,81 @@ const skillsList = [
 
   const currentYear = new Date().getFullYear();
   document.getElementById('currentYear').textContent = currentYear;
+
+  // Function to set the selected mode
+function setMode(mode) {
+    const linkElement = document.getElementById('themeCSS');
+  
+    // Update href based on the selected mode
+    if (mode === 'dark') {
+      linkElement.href = 'style.css'; // Replace 'dark.css' with your dark mode stylesheet path
+    } else if (mode === 'light') {
+      linkElement.href = 'light.css'; // Replace 'light.css' with your light mode stylesheet path
+    } else if (mode === 'color') {
+      linkElement.href = 'color.css'; // Replace 'color.css' with your color mode stylesheet path
+    }
+  }
+  
+  // Event listeners for mode selection buttons
+  document.getElementById('darkModeBtn').addEventListener('click', function() {
+    setMode('dark');
+  });
+  
+  document.getElementById('lightModeBtn').addEventListener('click', function() {
+    setMode('light');
+  });
+  
+  document.getElementById('colorModeBtn').addEventListener('click', function() {
+    setMode('color');
+  });
+
+const themeElement = document.getElementById('themeElement');
+
+themeElement.addEventListener('click', function() {
+  const themeBtn = this.querySelector('.theme-btn');
+  themeBtn.style.display = themeBtn.style.display === 'flex' ? 'none' : 'flex';
+});
+
+
+const themeTitle = document.querySelector('.theme-title');
+const darkModeBtn = document.getElementById('darkModeBtn');
+const lightModeBtn = document.getElementById('lightModeBtn');
+const colorModeBtn = document.getElementById('colorModeBtn');
+
+darkModeBtn.addEventListener('mouseover', function() {
+  themeTitle.textContent = 'Dark';
+});
+
+darkModeBtn.addEventListener('mouseout', function() {
+  themeTitle.textContent = 'Switch Theme';
+});
+
+lightModeBtn.addEventListener('mouseover', function() {
+  themeTitle.textContent = 'Light';
+});
+
+lightModeBtn.addEventListener('mouseout', function() {
+  themeTitle.textContent = 'Switch Theme';
+});
+
+colorModeBtn.addEventListener('mouseover', function() {
+  themeTitle.textContent = 'Color';
+});
+
+colorModeBtn.addEventListener('mouseout', function() {
+  themeTitle.textContent = 'Switch Theme';
+});
+
+const myImage = document.querySelector('.myimage img');
+
+darkModeBtn.addEventListener('click', function() {
+  myImage.src = './images/Cute Avatar.svg'; // Change image source for dark mode
+});
+
+lightModeBtn.addEventListener('click', function() {
+  myImage.src = './images/Cute Avatar2.svg'; // Change image source for light mode
+});
+
+colorModeBtn.addEventListener('click', function() {
+  myImage.src = './images/Cute Avatar3.svg'; // Change image source for color mode
+});
